12e476a3c156342880218d8835a6f824568e80c8
kadykov <62546709+kadykov@users.noreply.github.com>
2023-01-24 16:34:37 +0100

    Add basic pint(dask) array tests

--- cupy_xarray/tests/test_accessors.py
+++ cupy_xarray/tests/test_accessors.py
@@ -44,6 +44,19 @@ def tutorial_da_air_pint(tutorial_ds_air_pint):
     return tutorial_ds_air_pint.air
 
 
+@pytest.fixture
+def tutorial_ds_air_pint_dask():
+    return (
+        xr.tutorial.open_dataset("air_temperature", chunks={"lat": 25, "lon": 25, "time": -1})
+        * ureg.Quantity("degree_Kelvin")
+    )
+
+
+@pytest.fixture
+def tutorial_da_air_pint_dask(tutorial_ds_air_pint_dask):
+    return tutorial_ds_air_pint_dask.air
+
+
 def test_data_set_accessor(tutorial_ds_air):
     ds = tutorial_ds_air
     assert hasattr(ds, "cupy")
@@ -96,3 +109,21 @@ def test_data_array_accessor_pint(tutorial_da_air_pint):
 
     da = da.cupy.as_numpy()
     assert not da.cupy.is_cupy
+    assert isinstance(da.data, pint_array_type)
+
+
+@requires_pint
+def test_data_array_accessor_pint_dask(tutorial_da_air_pint_dask):
+    da = tutorial_da_air_pint_dask
+    assert hasattr(da, "cupy")
+    assert not da.cupy.is_cupy
+
+    da = da.as_cupy()
+    assert da.cupy.is_cupy
+    assert isinstance(da.data, pint_array_type)
+    assert isinstance(da.data.magnitude, dask_array_type)
+
+    da = da.cupy.as_numpy()
+    assert not da.cupy.is_cupy
+    assert isinstance(da.data, pint_array_type)
+    assert isinstance(da.data.magnitude, dask_array_type)
