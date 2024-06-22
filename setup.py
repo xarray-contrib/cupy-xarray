a9f5341d14a168e85a63e6824d2bbe3107315410
Wei Ji <23487320+weiji14@users.noreply.github.com>
2024-06-22 17:09:57 +1200

    Add pooch to test_requires
    Fixes `ModuleNotFoundError: No module named 'pooch'` when trying to call `xr.tutorial.open_dataset`.

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ setuptools.setup(
     ],
     python_requires=">=3.7",
     install_requires=requirements,
-    extras_require={"test": ["dask", "pytest"]},
+    extras_require={"test": ["dask", "pooch", "pytest"]},
 )
