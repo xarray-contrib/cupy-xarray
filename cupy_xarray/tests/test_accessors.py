126b0a9fb27415362e7b5642558709ab7ad92d7a
Wei Ji <23487320+weiji14@users.noreply.github.com>
2024-06-22 17:07:14 +1200

    Fix ModuleNotFoundError: No module named 'xarray.core.pycompat'
    Xref https://github.com/pydata/xarray/blob/v2024.06.0/xarray/namedarray/pycompat.py#L21-L43

--- cupy_xarray/tests/test_accessors.py
+++ cupy_xarray/tests/test_accessors.py
@@ -1,9 +1,12 @@
 import numpy as np
 import pytest
 import xarray as xr
-from xarray.core.pycompat import dask_array_type
+from xarray.namedarray.pycompat import DuckArrayModule
 
 import cupy_xarray  # noqa: F401
+
+dsk = DuckArrayModule("dask")
+dask_array_type = dsk.type
 
 
 @pytest.fixture
