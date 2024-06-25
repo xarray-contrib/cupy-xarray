fb0dd1821c14eccac6431f5fe1860363f40e80a4
Wei Ji <23487320+weiji14@users.noreply.github.com>
2024-06-25 13:30:31 +1200

    Keep setup.py with versioneer.get_version() and .get_cmdclass()
    Still need these two lines so that the sdist won't have a 0.0.0 version. Xref https://github.com/python-versioneer/python-versioneer/blob/0.29/INSTALL.md#common-steps

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+import versioneer
+
+if __name__ == "__main__":
+    setuptools.setup(
+        version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),
+    )
